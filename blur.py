44ab459d664af50d2f4b2273a3a655a528d60fd1
mars1198 <emperie2006@yandex.ru>
2020-07-05 16:16:37 +0200

    Create blur.py

---
+++ blur.py
@@ -0,0 +1,64 @@
+from PIL import Image, ImageFilter
+import glob
+import os
+
+
+
+def Blur(path, string):
+    for filename in glob.glob(string): #path of raw images
+       img = Image.open(filename)
+       imgSmall = img.resize((128,128),resample=Image.BILINEAR)
+       result = imgSmall.filter(ImageFilter.BLUR)
+    # save blured images to new folder with existing filename
+       result.save('{}{}{}'.format(path,'/',os.path.split(filename)[1]))
+
+
+path = 'blured/Ginia munda munda' #path to save
+string = 'images/Ginia munda munda/*.jpg' #path to open
+# create new folder
+if not os.path.exists(path):
+    os.makedirs(path)
+Blur(path, string)
+
+path = 'blured/Ginia sublitoralis'
+string = 'images/Ginia sublitoralis/*.jpg' 
+# create new folder
+if not os.path.exists(path):
+    os.makedirs(path)
+Blur(path, string)
+
+path = 'blured/Macedopyrgula pavlovici'
+string = 'images/Macedopyrgula pavlovici/*.jpg' 
+# create new folder
+if not os.path.exists(path):
+    os.makedirs(path)
+Blur(path, string)
+
+path = 'blured/Macedopyrgula wagneri'
+string = 'images/Macedopyrgula wagneri/*.jpg' 
+# create new folder
+if not os.path.exists(path):
+    os.makedirs(path)
+Blur(path, string)
+
+path = 'blured/Ohridopyrgula charensis'
+string = 'images/Ohridopyrgula charensis/*.jpg' 
+# create new folder
+if not os.path.exists(path):
+    os.makedirs(path)
+Blur(path, string)
+
+path = 'blured/Ohridopyrgula macedonica'
+string = 'images/Ohridopyrgula macedonica/*.jpg'
+# create new folder
+if not os.path.exists(path):
+    os.makedirs(path)
+Blur(path, string)
+
+path = 'blured/Ohridopyrgula svnaum'
+string = 'images/Ohridopyrgula svnaum/*.jpg' 
+# create new folder
+if not os.path.exists(path):
+    os.makedirs(path)
+Blur(path, string)
+
